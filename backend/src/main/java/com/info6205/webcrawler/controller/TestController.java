11799e201aa0891136bd59043d41ae07ead8bf2f
saxena-akshit <saxena.aksh@northeastern.edu>
2024-11-24 23:23:08 -0500

    chore: add test api

---
+++ backend/src/main/java/com/info6205/webcrawler/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.info6205.webcrawler.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping(value = "/test-api", method = RequestMethod.HEAD)
+    public ResponseEntity<Void> testApi() {
+        return ResponseEntity.ok().build();
+    }
+}
