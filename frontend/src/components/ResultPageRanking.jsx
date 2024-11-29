c9f1fbd8c1e2497131e1281bf8f4f88d4d1f8e31
Sanskrutii03 <manoria.s@northeastern.edu>
2024-11-29 05:15:44 -0500

    Added page ranking

--- frontend/src/components/ResultPageRanking.jsx
+++ frontend/src/components/ResultPageRanking.jsx
@@ -1,1 +1,25 @@
-//todo
+import responseData from "../model/api-response.json";
+import "./Results.css";
+
+function ResultPageRanking() {
+    const sortedData = [...responseData.data].sort((a, b) => b.in_degree - a.in_degree);
+
+    return (
+        <div className="ranking-section">
+            <h2>Page Ranking</h2>
+            <ul className="ranking-list">
+                {sortedData.map((item, index) => (
+                    <li key={index} className="ranking-item">
+                        <div className="url">{item.url}</div>
+                        <div className="details">
+                            <span>Rank: {item.rank}</span>
+                            <span>In-Degree: {item.in_degree}</span>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default ResultPageRanking;
