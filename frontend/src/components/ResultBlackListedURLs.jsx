6afce40ed8feb4c5e665ca87b271ed190e44fa3e
Sanskrutii03 <manoria.s@northeastern.edu>
2024-12-02 22:30:18 -0500

    static blacklisted URLs list

--- frontend/src/components/ResultBlackListedURLs.jsx
+++ frontend/src/components/ResultBlackListedURLs.jsx
@@ -2,6 +2,23 @@ import "./Results.css";
 
 function ResultBlackListedURLs() {
     const blacklistedUrls = [
+        "https://www.creativebookmark.com/",
+        "https://www.ffupdate.org",
+        "https://www.ad-tracker.example",
+        "https://www.popup-ads-site.net",
+        "https://www.bannerads.org",
+        "https://nu.outsystemsenterprise.com/FSD/",
+        "https://www.instagram.com/northeastern/",
+        "https://www.northeastern.edu/charlotte/",
+        "https://geo.northeastern.edu/blog/country/hong-kong/",
+        "https://www.tiktok.com/@northeasternu",
+        "https://research.northeastern.edu/cognitive-and-brain-health/",
+        "https://www.facebook.com",
+        "https://www.twitter.com",
+        "https://www.instagram.com",
+        "https://www.fonts.googleapis.com",
+        "https://www.google.com",
+        "https://www.youtube.com",
 
     ];
 
